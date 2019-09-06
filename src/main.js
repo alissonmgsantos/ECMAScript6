fd196379b0e90bca9bc7312bf93a18b0a58d690f
Alisson Matos <alissonmgsantos@gmail.com>
2019-09-06 16:58:43 -0300

    Cadastrando todo

--- src/main.js
+++ src/main.js
@@ -1,1 +1,27 @@
-alert('Testando webpack');
+class App {
+
+    constructor() {
+        this._repositories = [];
+        this._formElement = document.getElementById('repo-form');
+    }
+
+    registerHandlers() {
+        this._formElement.onsubmit = event => this.addRepository(event);
+    }
+
+
+    addRepository(event) {
+        
+        event.preventDefault();
+
+        this._repositories.push({
+            name: 'name',
+            description: 'description',
+            avatar_url: 'avatar_url',
+            html_url: 'html_url'
+        });
+        console.log(this._repositories);
+    }
+}
+
+new App();
